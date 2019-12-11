7e88732a34df6207afeeb73a0a47c04a573ec831
Ryan B <brockhoffrw@gmail.com>
2019-12-11 19:32:41 +0200

    feat: sign out functionality.

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { View, Text } from "native-base";
+import { StyleSheet } from "react-native";
+import { View, Text, Button } from "native-base";
+import { Auth } from "aws-amplify";
 
-const Dashboard = () => {
+const Dashboard = props => {
+  const handleSignOut = () => {
+    Auth.signOut()
+      .then(() => props.navigation.navigate("Authentication"))
+      .catch(err => console.log(err));
+  };
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Welcome to Dashboard!</Text>
+      <Button onPress={handleSignOut}>
+        <Text black block>
+          Sign Out
+        </Text>
+      </Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30
+  }
+});
 
 export default Dashboard;
